1ded5ff2cd5b2b92876bbb7deb1c5d3962ff008f
Stephen <ding.q.stephen@gmail.com>
2024-08-09 22:54:18 -0400

    Play Again button works and restarts the game. Need to add platform image at the start.

--- script.js
+++ script.js
@@ -5,15 +5,17 @@ let computerScore = 0;
     const computerContainer = document.querySelector("#computerChoice")
     const winLossOutput = document.querySelector(".first-row");
     const gameContainer = document.querySelector(".game-container");
-    const finalResultContainer = document.querySelector(".final-result-container");
     const input = document.querySelector(".gen-selector");
     const playerPokemonImage = document.querySelector("#playerPokemonImage");
     const computerPokemonImage = document.querySelector("#computerPokemonImage");
 
+    const finalResultContainer = document.querySelector(".final-result-container");
+    const finalResultText = document.querySelector(".final-result-text");
+    const playAgainButton = document.querySelector(".play-again-button");
+
     buttons.forEach((button) =>{
       button.addEventListener("click", () =>{
         const genNum = input.value;
-        console.log(genNum);
         if(genNum==""){
           alert("You must pick a generation!");
         }
@@ -45,8 +47,10 @@ let computerScore = 0;
         
         if (humanChoice != null) {
             humanChoice.toLowerCase();
-            playerContainer.innerText = `You chose: ${humanChoice}`;
-            computerContainer.innerText = `AI chose: ${computerChoice}`;
+            const tempHumanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
+            const tempComputerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+            playerContainer.innerText = `You chose: ${tempHumanChoice} \n`;
+            computerContainer.innerText = `AI chose: ${tempComputerChoice} \n`;
             changeImage(humanChoice, computerChoice, genNum);
         }
 
@@ -56,16 +60,10 @@ let computerScore = 0;
           (humanChoice == "grass" && computerChoice == "fire")
         ) {
               humanScore++;
-              winLossOutput.innerText = "You win! " + humanChoice + " beats " + computerChoice + "!";
-              //const buttonColor = document.getElementById(`#${humanChoice}`).style.backgroundColor;  
+              winLossOutput.innerText = `You win! ${humanChoice} beats ${computerChoice}!`;
               const buttonColor = window.getComputedStyle(document.querySelector(`#${humanChoice}`)).backgroundColor;
-              console.log(buttonColor);
               winLossOutput.setAttribute("style", `background-color: ${buttonColor}`);
-              console.log(
-                "You win! " + humanChoice + " beats " + computerChoice + "!"
-              );
-              console.log("Human score = " + humanScore);
-              console.log("Computer score = " + computerScore);
+
         } 
         
         else if (
@@ -74,24 +72,14 @@ let computerScore = 0;
           (humanChoice == "water" && computerChoice == "water")
         ) {
           
-          winLossOutput.innerText = "Tie! You both picked " + humanChoice +"!";
+          winLossOutput.innerText = `Tie! You both picked ${humanChoice}!`;
           winLossOutput.setAttribute("style", `background-color: grey`);
-              console.log(
-                "Tie! You both picked " +
-                  humanChoice +
-                  ". This round doesn't count. Try again!"
-              );
         } 
         
         else {
               computerScore++;
-              winLossOutput.innerText = "You lose! " + computerChoice + " beats " + humanChoice + "!";
+              winLossOutput.innerText = `You lose! ${computerChoice} beats ${humanChoice}!`;
               winLossOutput.setAttribute("style", `background-color: grey`);
-              console.log(
-                "You lose! " + computerChoice + " beats " + humanChoice + "!"
-              );
-              console.log("Human score = " + humanScore);
-              console.log("Computer score = " + computerScore);
         }
 
        const humanScoreLabel = document.createElement("div");
@@ -110,17 +98,33 @@ let computerScore = 0;
       toggleElements();
       console.log("Should be toggling.");
       const result = humanScore === 5 ? "won" : "lose";
-      finalResultText.innerTexts = `You ${result}! ${humanScore}-${computerScore}`;
+      finalResultText.innerText = `You ${result}! \n ${humanScore}-${computerScore}`;
 
     }
   
     function toggleElements(){
       gameContainer.classList.toggle("hidden");
       finalResultContainer.classList.toggle("hidden");
-      
+      playAgainButton.classList.toggle("hidden");
     }
 
     function changeImage(humanChoice, computerChoice, genNum){
       playerPokemonImage.src = `./starter-images/gen-${genNum}-${humanChoice}.png`;
       computerPokemonImage.src = `./starter-images/gen-${genNum}-${computerChoice}.png`;
+    }
+
+    playAgainButton.addEventListener("click", reset);
+
+    function reset(){
+      toggleElements();
+      humanScore =0;
+      computerScore = 0;
+      playerPokemonImage.src = "";
+      computerPokemonImage.src = "";
+      winLossOutput.innerText = "Let's Battle!";
+      winLossOutput.setAttribute("style", `background-color: rgb(232, 118, 118)`);
+      playerContainer.innerText = "Choose a Pokemon!";
+      computerContainer.innerText = "Choose a Pokemon!";
+      input.value = "";
+      input.focus();
     }
